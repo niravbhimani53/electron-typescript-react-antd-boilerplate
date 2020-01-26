e1f7ac461458a051cc382f2b86f5b81405cbd719
Nirav Bhimani <niravbhimani53@gmail.com>
2020-01-26 18:06:14 +1100

    Initial boilerplate setup

---
+++ src/renderer/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { hot } from "react-hot-loader/root";
+import "antd/dist/antd.css";
+
+function App() {
+  return <div>Hello from RFC!</div>;
+}
+
+export default hot(App);
